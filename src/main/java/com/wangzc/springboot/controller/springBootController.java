bc5c31082492785ff104bc6fb9e7a2f0b899ac56
亡羊啊十大科技 <2380882530@qq.com>
2020-03-31 14:39:58 +0800

    这是个测试项目，提交到gitHub

---
+++ src/main/java/com/wangzc/springboot/controller/springBootController.java
@@ -0,0 +1,17 @@
+package com.wangzc.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class springBootController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String getShow(){
+        System.out.println("这是个测试git");
+        return "Hello World";
+    }
+
+}
